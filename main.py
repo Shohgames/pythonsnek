3b6abe0e59eb2d3d7b6f833cd97a88bc08c8c4d3
GayLordBanana21241 <sjv22001@student.mdu.se>
2025-08-13 14:30:52 +0900

    board renderbasic code

---
+++ main.py
@@ -0,0 +1,36 @@
+#main code for the snake game
+class Snake:
+    pass
+
+class Apple:
+    pass
+
+
+class Game:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+    
+    def board_matrix(self):
+        # Matrix filled with "None"
+        # Here it will return a 2d list of self.height and self.width with None elemnetns
+        return [[None for _ in range(self.width)] for _ in range(self.height)]
+    
+    def render(self):
+        matrix = self.board_matrix()
+        #Prints the top border, it makes the first corner with + and then makes - times whatever and adds the final +
+        print("+" + "-" * self.width + "+")
+        #print each row with the same logic
+        for row in matrix:
+            print("|" + "" * self.width + "|")
+        #print bottom border with the same logic
+        print("+" + "-" * self.width + "+")
+
+        #height and width printer below for debug, no need to print it ingame
+        #print("Height:", self.height)
+        #print("Width:", self.width)
+
+game = Game(10, 20)
+game.render()
+
+
