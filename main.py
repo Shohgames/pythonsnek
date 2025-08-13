1533ec119a0fe4aabfc1c6e514bb45db37cce51c
GayLordBanana21241 <sjv22001@student.mdu.se>
2025-08-13 15:09:08 +0900

    added the snake and basic board functin

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 #main code for the snake game
-class Snake:
-    pass
 
+UP = (0,1)
+DOWN = (0, -1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+class Snake:
+    def __init__(self, initbody, initdirection):
+        self.body = initbody #initital body from the tuple
+        self.direction = initdirection 
+    def take_step(self, position):
+        self.body = [position] + self.body[:-1] #when executed, it slices the end of the snake and puts it forward. 
+    def set_direction(self, direction):
+        self.direction = direction
+    def head(self):
+        
 class Apple:
     pass
 
@@ -10,11 +23,18 @@ class Game:
     def __init__(self, height, width):
         self.height = height
         self.width = width
+        self.snake = Snake([(0,0),(1,0),(2,0),(3,0)], UP)
+
     
     def board_matrix(self):
-        # Matrix filled with "None"
-        # Here it will return a 2d list of self.height and self.width with None elemnetns
+        #Matrix filled with "None"
+        #Here it will return a 2d list of self.height and self.width with None elemnetns
         return [[None for _ in range(self.width)] for _ in range(self.height)]
+        #Draw the snake
+        for pos in self.snake.body[1:]:
+            x, y = pos
+            if 0 <= y self.height and 0 <= x < self.width:
+                    matrix[y][x] = "0"
     
     def render(self):
         matrix = self.board_matrix()
@@ -32,7 +52,7 @@ class Game:
         #print("Height:", self.height)
         #print("Width:", self.width)
 
-game = Game(10, 20)
+game = Game(10, 25)
 game.render()
 
 
